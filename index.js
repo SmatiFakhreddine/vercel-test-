e1b54223e22b6a1fbb955c3aefff61c861ec325b
SmatiFakhreddine <smatifakhri9@gmail.com>
2025-06-07 19:21:24 +0100

    add envirment variable

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser";
+import env from "dotenv";
+
+env.config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.set('views', './views');
 app.use(express.static("public"));
